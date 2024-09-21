53da170d05766dac8ebbc27185f4ce0c35622631
Sergey T <forl86@gmail.com>
2024-09-21 15:32:03 +0800

    exclude .idea

--- src/main/java/org/example/springbootstart/SpringBootStartApplication.java
+++ src/main/java/org/example/springbootstart/SpringBootStartApplication.java
@@ -1,8 +1,5 @@
 package org.example.springbootstart;
 
-import org.example.springbootstart.dao.UserDao;
-import org.example.springbootstart.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
